85be724a3e4775095342c15401b4d1c962a9c8f2
alatar- <contact@alatar.eu>
2017-01-31 16:02:42 +0100

    Implement TinyUrl generation.

--- index/urls.py
+++ index/urls.py
@@ -5,6 +5,7 @@ app_name = 'index'
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^generate/?$', views.generate, name='generate'),
     url(r'^(?P<tiny_url>[A-z0-9]{3,9})/?$', views.redirection, name='redirection'),
     url(r'^!(?P<tiny_url>[A-z0-9]{3,9})/?$', views.detail, name='detail'),
 ]
--- index/views.py
+++ index/views.py
@@ -1,16 +1,51 @@
+import string
+import random
+
 from django.shortcuts import get_object_or_404, render, redirect
+from django.utils import timezone
+from django.contrib import messages
+
 from .models import Url
 
 
 def index(request):
     '''Main view that enables creation of TinyUrl's'''
-    context = {'created_url': False}
-    return render(request, 'index/index.html', context)
+    tiny_url = request.session.pop('tiny_url', None)
+    return render(request, 'index/index.html', {'tiny_url': tiny_url})
+
+
+def generate(request):
+    if not request.POST['destination_url']:
+        messages.error(request, "Please specify a destination_url.")
+        return redirect('index:index')
+
+    def rand_tiny_url():
+        length = random.choice(range(3, 8))
+        symbols = (string.ascii_uppercase + string.digits)
+        tiny_url = ''.join(random.choice(symbols) for _ in range(length))
+        return tiny_url
+    tiny_url = rand_tiny_url()
+    while True:
+        # TODO: this should be refactored to provide fixed generation time
+        # for each request, e.g. use predefined list of available urls or
+        # pre-generate few urls.
+        if not Url.objects.filter(tiny_url=tiny_url).exists():
+            break
+        tiny_url = rand_tiny_url()
+
+    url = Url(destination_url=request.POST['destination_url'],
+              tiny_url=tiny_url,
+              pub_date=timezone.now())
+    url.save()
+    request.session['tiny_url'] = url.tiny_url
+    return redirect('index:index')
 
 
 def redirection(request, tiny_url):
     '''View redirecting from tiny_url to destination_url'''
     url = get_object_or_404(Url, tiny_url=tiny_url)
+    url.visit_counter += 1
+    url.save()
     return redirect(url.destination_url)
 
 
