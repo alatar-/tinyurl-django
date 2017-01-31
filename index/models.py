efb4a85013524e99e874407cf7d835fb68bdc37b
alatar- <contact@alatar.eu>
2017-01-31 14:01:41 +0100

    Add details view, update urls.

--- index/models.py
+++ index/models.py
@@ -3,6 +3,6 @@ from django.db import models
 
 class Url(models.Model):
     destination_url = models.URLField('destination url')
-    tiny_url_text = models.CharField('tiny url', max_length=10)
+    tiny_url = models.CharField('tiny url', max_length=10)
     pub_date = models.DateTimeField('date published')
     visit_counter = models.IntegerField('visit counter', default=0)
--- index/views.py
+++ index/views.py
@@ -1,5 +1,12 @@
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from .models import Url
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    context = {'created_url': False}
+    return render(request, 'index/index.html', context)
+
+
+def detail(request, tiny_url):
+    get_object_or_404(Url, tiny_url=tiny_url)
+    return render(request, 'index/detail.html', {'url': tiny_url})
