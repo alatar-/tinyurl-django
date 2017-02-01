582add425963cc9c467b4067ea3f9018dd457d59
alatar- <contact@alatar.eu>
2017-02-01 15:30:09 +0100

    Cleaup project structure.

--- tinyurl/settings.py
+++ tinyurl/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'index.apps.IndexConfig',
-    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
